d9061f07b0594939d52e64ac990f9f194ec1bf2d
AngelNahuat <nglnahuat@gmail.com>
2022-02-19 18:34:11 -0500

    MVC

--- app/src/main/java/com/example/layouts/MainActivity.kt
+++ app/src/main/java/com/example/layouts/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.layouts
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val saludar=findViewById<RelativeLayout>(R.id.btnComprobar)
+        val nombre=findViewById<EditText>(R.id.etNombre)
+        val cb=findViewById<CheckBox>(R.id.cbSoy)
+        saludar.setOnClickListener(View.OnClickListener {
+            if(Validar()){
+                if(cb.isChecked){
+                    Toast.makeText(this,"hola"+nombre.text+"desarrollador",Toast.LENGTH_LONG).show()
+                }else
+
+                Toast.makeText(this,"hola"+nombre.text,Toast.LENGTH_LONG).show()
+            }
+        else{
+            Toast.makeText(this,"escribe tu nombre",Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+    fun Validar():Boolean{
+        val etNombre=findViewById<EditText>(R.id.etNombre)
+        val NombreUsu=etNombre.text
+        if(NombreUsu.isNullOrEmpty()){
+            return false
+        }
+
+        return true
     }
 }
